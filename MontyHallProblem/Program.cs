5c4384524577fcd32431833236bd80934ff28db9
Lena-hal <olo.ha@seznam.cz>
2024-04-17 10:22:11 +0200

    initial commit

---
+++ MontyHallProblem/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace MontyHallProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Run the simulation 1,000,000 times
+            int wins = 0;
+            int losses = 0;
+            for (int i = 1; i <= 1000000; i++)
+            {
+                bool success = MontyHall();
+                if (success)
+                {
+                    wins++;
+                }
+                else
+                {
+                    losses++;
+                }
+            // if the simluation has been run 10^N times, output the results
+            // it has to be dynamic to any number
+            if (Math.Log10(i) % 1 == 0)
+                {
+                Console.WriteLine("Wins: " + wins);
+                Console.WriteLine("Losses: " + losses);
+                double winPercentage = (double)wins / (double)(wins + losses) * 100;
+                Console.WriteLine("Win Percentage: " + winPercentage + "%");
+                }
+
+            }
+        }
+        public static bool MontyHall()
+        {
+            // Initialize the doors and fills them with goats (false)
+            bool[] doors = new bool[3];
+            doors[0] = false;
+            doors[1] = false;
+            doors[2] = false;
+            Random random = new Random();
+            // Place the car behind one of the doors (true)
+            int car = random.Next(0, 3);
+            doors[car] = true;
+            // The player selects a door
+            int choice = random.Next(0, 3);
+
+            // Monty opens a door that has not been selected and does not have the car
+            int montyOpen = random.Next(0, 3);
+            while (montyOpen == choice || doors[montyOpen] == true)
+            {
+                montyOpen = random.Next(0, 3);
+            }
+            // player switches doors
+            if (choice == 0 && montyOpen == 1)
+            {
+                choice = 2;
+            }
+            else if (choice == 0 && montyOpen == 2)
+            {
+                choice = 1;
+            }
+            else if (choice == 1 && montyOpen == 0)
+            {
+                choice = 2;
+            }
+            else if (choice == 1 && montyOpen == 2)
+            {
+                choice = 0;
+            }
+            else if (choice == 2 && montyOpen == 0)
+            {
+                choice = 1;
+            }
+            else if (choice == 2 && montyOpen == 1)
+            {
+                choice = 0;
+            }
+            // Check if the player won
+            if (doors[choice] == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
---
+++ MontyHallProblem/README.md
@@ -0,0 +1,29 @@
+﻿# Monty Hall Problem Simulation
+I recently found out about the monty hall problem and i was intrigued by it. I felt like it doesnt make sense so i decided to write this program to simulate the problem and see if it makes sense.
+This script runs the simulation 1 Milion times and calculates the probability of winning if you switch doors.
+these were the results:
+```
+Wins: 1
+Losses: 0
+Win Percentage: 100%
+Wins: 7
+Losses: 3
+Win Percentage: 70%
+Wins: 68
+Losses: 32
+Win Percentage: 68%
+Wins: 688
+Losses: 312
+Win Percentage: 68.8%
+Wins: 6689
+Losses: 3311
+Win Percentage: 66.89%
+Wins: 66620
+Losses: 33380
+Win Percentage: 66.62%
+Wins: 666590
+Losses: 333410
+Win Percentage: 66.659%
+```
+From the data i can confirm that the probability of winning if you switch doors is 66.6% which is a significant increase from the 33.3% if you dont switch doors.
+This means that the monty hall problem is correct and it is better to switch doors.
